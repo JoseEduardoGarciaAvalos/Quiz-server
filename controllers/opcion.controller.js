422a37da4a7f55180671f5dba80f3f64517b8df1
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2021-04-07 10:42:57 -0600

    Refactory name method of Opcion

--- controllers/opcion.controller.js
+++ controllers/opcion.controller.js
@@ -1,6 +1,6 @@
 var Opcion = require("../models/opcion");
 
-exports.opcion_get_by_codePregunta = function (req, res) {
+exports.getOpcionesByCodePregunta = function (req, res) {
     let code= req.params.code;
     Opcion.findByPregunta(code, (err, opciones) => {
         res.status(200).json(
--- routes/opcion.route.js
+++ routes/opcion.route.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var opcionController = require("../controllers/opcion.controller");
 
-router.get('/:code', opcionController.opcion_get_by_codePregunta);
+router.get('/:code', opcionController.getOpcionesByCodePregunta);
 //router.post('/:code/update', preguntaController.pregunta_get);
 
 module.exports = router;
